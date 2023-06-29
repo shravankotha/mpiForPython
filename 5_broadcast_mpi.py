7b041597f299d67df2e16ba4d10e0f5b44df0a05
shravan kotha <shravan.kotha@icmrl.net>
2023-06-29 14:24:15 -0400

    array is broadcasted from root to all

--- 5_broadcast_mpi.py
+++ 5_broadcast_mpi.py
@@ -2,13 +2,16 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
-print("My rank is : " + str(rank))
-root_process_rank = 3
+#print("My rank is : " + str(rank))
+root_process_rank = 0
 
 if rank == root_process_rank:
-    data_to_send = "a"
+    data_to_send = [1,2,3]
 else:
     data_to_send = None
     
 variable_shared = comm.bcast(data_to_send, root = root_process_rank)    # shares the data to all processors including itself
-print("Process : " + str(rank) + " shared data : " + str(variable_shared))
+print("Process : ", rank, " shared data : ",variable_shared)
+
+
+# bcast is collective, i.e. all the processes must call it
